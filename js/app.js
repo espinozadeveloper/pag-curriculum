1321350ce469beb7007c479262120a6bcbb36ade
espinozadeveloper <espinoza.developer@gmail.com>
2023-01-12 12:03:53 -0400

    Implementar reCaptcha

--- js/app.js
+++ js/app.js
@@ -48,16 +48,23 @@ $form.addEventListener('submit', handleSubmit);
 
 async function handleSubmit(event){
     event.preventDefault();
-    const form = new FormData(this);
-    const response = await fetch(this.action, {
-        method: this.method,
-        body: form,
-        headers:{
-            'Accept': 'application/json'
-        } 
-    });
-    if (response.ok){
-        this.reset();
-        swal('Mensaje enviado','Gracias por contactarme','success');
+    var response = grecaptcha.getResponse();
+    if (response.length == 0){
+        swal('Mensaje no enviado','Por favor indica que no eres un robot','error');
+        return false;
+    }
+    else{
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
+            method: this.method,
+            body: form,
+            headers:{
+                'Accept': 'application/json'
+            } 
+        });
+        if (response.ok){
+            this.reset();
+            swal('Mensaje enviado','Gracias por contactarme','success');
+        }
     }
 }
